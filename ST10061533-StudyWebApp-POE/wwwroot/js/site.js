9cf1f1c083c96d97597689698a0dfbfeb0a35dd3
BonganiChauke <bonganichauke11@outlook.com>
2025-07-02 15:48:43 +0200

    Add smooth scrolling to navigation links
    Implemented a feature that allows smooth scrolling to target elements when clicking on links with the class `nav-link`. The default action is prevented, enhancing user experience by providing a seamless navigation effect.

--- ST10061533-StudyWebApp-POE/wwwroot/js/site.js
+++ ST10061533-StudyWebApp-POE/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
